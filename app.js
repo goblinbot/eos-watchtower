e38f3bc660fee34c62ea933136f4169b64a4fec9
Thijs Boerma <thijs.boerma@sogeti.com>
2018-10-11 15:14:26 +0200

    Start with express test for openshift

---
+++ app.js
@@ -0,0 +1,24 @@
+// Het is kwart voor Express, pak express en de service(s).
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+// const storeService = require('/server/services/storeService'); // niet meer gebruiken voor nu.
+
+// Het is Express-'o-clock.
+const app = express();
+
+// express configuratie
+app.use(bodyParser.json());
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// default bericht bij aanroepen API
+app.get('/', function (req, res) {
+    res.send('Welcome to the petSupplies API');
+});
+
+// Zet server aan bij 'node app','node app.js', 'nodemon app'..
+const port = process.env.PORT || 3000;
+const server = app.listen(port, function () {
+    console.log(`Listen OK: port ${port}`);
+});
