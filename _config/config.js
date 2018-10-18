e00dd32e4b72344a28ff695e2878b79bf72b6c89
Thijs Boerma <thijs.boerma@sogeti.com>
2018-10-18 13:08:48 +0200

    Routing: First Status API call testing + Setting config

--- _config/config.js
+++ _config/config.js
@@ -1,1 +1,21 @@
-// configuration file for eos-watchtower. Coming soonTM
+// dependancy
+const ip = require('ip');
+
+// configuration file for eos-watchtower.
+// This file can be modified to, for example, start watchtower on a different port, change the name, etc.
+
+systemSettings = {
+    port: 5000,
+    name: 'watchTower',
+    localaddress: ip.address(),
+}
+
+
+
+
+
+
+
+
+// export the config variables under a shorter name.
+exports.sys = systemSettings;
