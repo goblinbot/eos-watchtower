c4d7cd41a2c67c4c8eec43c7777b8971a7aa844c
Thijs Boerma <thijs.boerma@sogeti.com>
2019-03-22 14:47:36 +0100

    Enabled FOB update routes/logic

--- lib/routes/fob.routes.ts
+++ lib/routes/fob.routes.ts
@@ -14,6 +14,11 @@ export class FobRoutes {
             res.status(200).send({ message: 'MockFobs == go' });
         });
 
+        router.route('/update').post((req, res) => {
+            fobController.updateFob(req.body);
+            res.status(200).send({ message: 'Fob editted' });
+        });
+
         console.log('[RO] ..FoB Routes added.');
         return router;
     }
