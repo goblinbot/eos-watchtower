7a79012647b71c4337eba30bb59c06fd539776cb
Thijs Boerma <thijs.boerma@sogeti.com>
2019-08-14 12:36:40 +0200

    potential CORS fix

--- lib/modules/mission/mission.controller.ts
+++ lib/modules/mission/mission.controller.ts
@@ -48,4 +48,5 @@ export class MissionController {
             res.json([]);
         });
     }
+
 }
--- lib/modules/shared/filter.service.ts
+++ lib/modules/shared/filter.service.ts
@@ -1,28 +1,17 @@
-import * as blns from '../../bin/data/blns.json';
 import * as emojistrip from 'emoji-strip';
 import * as stripTags from 'striptags';
 
 export class FilterService {
 
     /**
-     * @description converts incoming string to string without the defined symbols.
-     * Please note that this filter is very unclean & brutal.
-     */
-    public removeSymbolsFromString(input: string): string {
-        return input.replace(/[`~!@#$%^&*()_|+=?;:'",<>\{\}\[\]\\\/]/gi, '');
-    }
-
-    /**
      * @alias 'silvesterfilter'
-     * removes Emoji's from a string.
-     * @returns {string} */
+     * removes Emoji's from a string. */
     public removeEmojisFromString(input: string): string {
         return emojistrip(input);
     }
 
     /**
      * @description strips HTML from an input string.
-     * @returns {string}
      */
     public stripHtml(input: string): string {
         return stripTags(input);
