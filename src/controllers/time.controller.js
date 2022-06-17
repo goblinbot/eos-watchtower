1f5d76760a7c35630cce776fa519a2848f593fdc
Thijs Boerma <th.boerma@gmail.com>
2022-06-17 10:50:59 +0200

    Refactor: reorganize and rename folder/file structure

--- src/controllers/time.controller.js
+++ src/controllers/time.controller.js
@@ -1,5 +1,1 @@
-const dayjs = require('dayjs');
-const UTC = require('dayjs/plugin/utc');
-const _config = require('../configs/config.json');
-
-dayjs.extend(UTC);
+const { Router } = require('express');
---
+++ src/services/time.service.js
@@ -0,0 +1,4 @@
+const dayjs = require('dayjs');
+const UTC = require('dayjs/plugin/utc');
+// const _config = require('../configs/config.json');
+dayjs.extend(UTC);
